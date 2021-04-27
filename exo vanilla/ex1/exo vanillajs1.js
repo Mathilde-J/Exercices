8dc17055691e92d9905e740665ccf5c6347d3bef
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2021-04-27 22:32:35 +0200

    début refactorisation du code

--- exo vanilla/ex1/exo vanillajs1.js
+++ exo vanilla/ex1/exo vanillajs1.js
@@ -1,26 +1,29 @@
-
-window.addEventListener("keydown", function(event){
-  
-    let audio = document.querySelector(`audio[data-key="${event.key}"]`);
-    let key = document.querySelector(`.key[data-key="${event.key}"]`) //selectionne par class avec le "."
+//Quand on touche certaines touches du clavier, un son est émis
+let AudiobyKey= (audio, key )=>{
     if(!audio) return;//si audio == false (!audio) la fonction s'arrete
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-    
-    
-    // while(audio.currentTime < audio.duration)
-    
-    if (audio.currentTime >0){
-        key.classList.remove('playing');
-        audio.pause();
-        audio.currentTime = 0;
-        return   
-    }  
-    // key.classList.remove('playing');
+}
 
-    //  console.dir(audio);// Affiche une liste interactive des propriétés de l’objet Javascript spécifié. La sortie est présenté comme un listing hiérarchique avec des triangles qui permettent l’affichage du contenu des objets-enfants.    
-} )
+
+window.addEventListener("keydown", (event)=>{
+    let audio = document.querySelector(`audio[data-key="${event.key}"]`);
+    let key = document.querySelector(`.key[data-key="${event.key}"]`);//selectionne par class avec le "."
+    AudiobyKey(audio, key);
+});
+
+//appuyer sur la touche relance l'audio
+//création d'un bouton stop pour arreter tout son 
+// if (audio.currentTime >0){
+//     console.log("merde")
+//     key.classList.remove('playing');
+//     audio.pause();
+//     audio.currentTime = 0;
+//     return   
+// }
+//  console.dir(audio);// Affiche une liste interactive des propriétés de l’objet Javascript spécifié. La sortie est présenté comme un listing hiérarchique avec des triangles qui permettent l’affichage du contenu des objets-enfants.    
+
 
 
 // test pour faire enlever l'animation
